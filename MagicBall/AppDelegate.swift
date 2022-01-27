da8957ad9090c48a87c64ecd85ca6c46b7628473
Andrey <boyko8120@gmail.com>
2022-01-27 21:58:06 +0200

    start

---
+++ MagicBall/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MagicBall
+//
+//  Created by Andrey Boyko on 27.01.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
